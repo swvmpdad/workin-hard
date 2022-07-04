62754513cc01b091b17b69747045589635fb62ec
swvmpdad <will.g.camp@gmail.com>
2022-07-04 16:10:31 -0500

    partially added local storage

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,35 +1,89 @@
+var plans = {
+    hour0: ["hello"],
+    hour1: ["hi"],
+    hour2: ["hey"],
+    hour3: [],
+    hour4: [],
+    hour5: [],
+    hour6: [],
+    hour7: [],
+    hour8: []
+};
+
+var createPlans = function() {
+    
+}
+
+var loadPlans = function() {
+    plans = JSON.parse(localStorage.getItem("plans"));
+
+    if(!plans) {
+        plans = {
+            hour9: [],
+            hour10: [],
+            hour11: [],
+            hour12: [],
+            hour1: [],
+            hour2: [],
+            hour3: [],
+            hour4: [],
+            hour5: []
+        };
+    }
+
+    $.each(plans, function(list, arr) {
+        console.log(list, arr);
+    })
+};
+
+var savePlans = function() {
+    localStorage.setItem("plans", JSON.stringify(plans));
+}
+
 // Set the current day at the top of the page
 var currentDay = function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
 }
 
-$(".textarea").on("click", function(){
+// make the hours empty div editable
+$(".planner").on("click", function(){
     var text = $(this)
         .text()
         .trim();
     var textInput = $("<textarea />")
-        .addClass("textarea col-9 d-flex")
+        .addClass("textarea col-9 d-flex border border-3 border-dark planner")
         .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
-    console.log(text);
 });
 
-$(".textarea").on("blur", "textarea", function(){
+// turn the text area back to a div
+$(".planner").off("focus", function(){
     var text = $(this)
-        .val()
-        .trim();
+    .val()
+    .trim();
 
-    var status = $(this)
-        .closest(".planner")
-        .attr("id")
-        .replace("hour-", "");
+var status = $(this)
+    .closest(".planner")
+    .attr("id")
+    .replace("hour-", "");
 
-    var planDiv = $("<div>")
-        .addClass("col-9 d-flex bg-light border border-3 border-dark planner")
-        .text(text);
+var index = $(this)
+    .closest(".time-block")
+    .index();
 
-    $(this).replaceWith(planDiv);
+    plans[status][index].text = text;
+
+var planDiv = $("<div>")
+    .addClass("col-9 d-flex bg-light border border-3 border-dark planner")
+    .text(text);
+
+$(this).replaceWith(planDiv);
+});
+
+$(".saveBtn").click(function(){
+savePlans();
 });
 
 currentDay();
+loadPlans();
