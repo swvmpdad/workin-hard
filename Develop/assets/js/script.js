bef5781639a8ce21b244654e3b02ab9e59e660f6
swvmpdad <will.g.camp@gmail.com>
2022-07-06 22:52:15 -0500

    finished localStorage functionality and created simple event listener for save buttons

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,42 +1,10 @@
 var plans = [];
 
-var createPlans = function() {
-    
-};
-
-var saveButtons = function() {
-    $("#btn-0").on("click", () => {
-        savePlans(0);
-    });
-    $("#btn-1").on("click", () => {
-        savePlans(1);
-    });
-    $("#btn-2").on("click", () => {
-        savePlans(2);
-    });
-    $("#btn-3").on("click", () => {
-        savePlans(3);
-    });
-    $("#btn-4").on("click", () => {
-        savePlans(4);
-    });
-    $("#btn-5").on("click", () => {
-        savePlans(5);
-    });
-    $("#btn-6").on("click", () => {
-        savePlans(6);
-    });
-    $("#btn-7").on("click", () => {
-        savePlans(7);
-    });
-    $("#btn-8").on("click", () => {
-        savePlans(8);
-    });
-};
-
+// load the plans on page start
 var loadPlans = function() {
     plans = JSON.parse(localStorage.getItem("plans"));
 
+    // if localStorage is empty, it will populate it with an empty array for "plans"
     if(!plans) {
         plans = {
             0: "",
@@ -52,20 +20,21 @@ var loadPlans = function() {
         localStorage.setItem("plans", JSON.stringify(plans));
     }
 
-    $.each(plans, function(list, arr) {
-        console.log(list, arr);
-    })
-
-    // for (var i = 0; i < plans.length; i++) {
-    //     var currentPlan = plans.i.val();
-    //     $("#hour-" + i).val() = currentPlan;
-    // }
+    // Populate time slots with saved tasks from local storage
+    for (var i = 0; i < 9; i++) {
+        var currentPlan = plans[i];
+        var textHourEl = $("#hour-"+i);
+        if (plans[i]) {
+        textHourEl.text(currentPlan);
+    }
+}
 };
 
+// save the specific hour's tasked that was clicked into localStorage
 var savePlans = function(id) {
-    textInput = $("textarea").val();
+    var plans = JSON.parse(localStorage.getItem("plans"));
+    var textInput = $("#hour-"+id).val();
     console.log(textInput);
-    plans = JSON.parse(localStorage.getItem("events"));
     plans[id] = textInput;
     localStorage.setItem("plans", JSON.stringify(plans));
 };
@@ -92,34 +61,13 @@ $(".planner").on("click", function(){
     textInput.trigger("focus");
 });
 
-// turn the text area back to a div
-$(".planner").off("focus", function(){
-    var text = $(this)
-    .val()
-    .trim();
-
-var status = $(this)
-    .closest(".planner")
-    .attr("id")
-    .replace("hour-", "");
-
-var index = $(this)
-    .closest(".time-block")
-    .index();
-
-    plans[status][index].text = text;
-
-var planDiv = $("<div>")
-    .addClass("col-9 d-flex bg-light border border-3 border-dark planner")
-    .text(text);
-
-$(this).replaceWith(planDiv);
-});
-
+// event handler for saving
 $(".saveBtn").click(function(){
-savePlans();
+    buttonId = $(this)
+        .attr("id")
+        .replace("btn-", "");
+    savePlans(buttonId);
 });
 
 currentDay();
 loadPlans();
-saveButtons();
